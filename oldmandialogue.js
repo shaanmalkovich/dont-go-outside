e3bd0e323b240cba4ecd77c297726530975a39b5
shaanmalkovich <65307797+shaanmalkovich@users.noreply.github.com>
2023-10-09 12:19:34 -0400

    Update oldmandialogue.js

--- oldmandialogue.js
+++ oldmandialogue.js
@@ -30,10 +30,34 @@ let club_lines = {
     line4: bye
 }
 
+let airport_talk = {
+  lineOne: "I get nervous on flights. A lot can go wrong between boarding and disembarking.",
+  lineTwo: "But it mostly falls on the pilot knowing what they're doing.",
+  lineThree: "Enjoy your flight"
+}
 
-Club - 
-Airport - "First time?" "I get nervous on flights. So much rides on you getting to your destination, but it mostly falls on the pilot knowing what they're doing. Enjoy your flight"
-Beach - "First time?" "If you decide to swim, watch out. The sharks here are lethal"
+let airport_lines = {
+  line1: firstTime,
+  line2: airport_talk.lineOne,
+  line3: airport_talk.lineTwo,
+  line4: airport_talk.lineThree,
+  line5: bye
+}
+
+let beach_talk = {
+  lineOne: "Beautiful day out. Lots of people had the same idea, but it's not too crowded.",
+  lineTwo: "If it gets too hot, you may find it tempting to take a dip in the ocean.",
+  lineThree: "If you decide to swim, watch out. Sharks are more daring around these waters, or so I hear."
+}
+
+let beach_lines = {
+  line1: beach_talk.lineOne,
+  line2: beach_talk.lineTwo,
+  line3: beach_talk.lineThree,
+  line: "Enjoy"
+}
+
+
 Rave - "First time?" "
 Chipotle - "First time?"
 Subway - "First time?"

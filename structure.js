2328ef1f229dd38e4a98f6c0e12015a147624d9c
shaanmalkovich <65307797+shaanmalkovich@users.noreply.github.com>
2023-10-09 02:22:41 -0400

    Update structure.js

--- structure.js
+++ structure.js
@@ -5,8 +5,10 @@ let choice = prompt("Where would you like to go today? ")
 
 startUp();
 
-let messageOne = "You wake up to your alarm at 9:47 on a Sunday morning."
-let messageTwo = "Through half-open eyes, you spy sunlight shining through the blinds."
-let messageThree = "What a beautiful day to go outside."
-
-let startMsg = `${messageOne} ${messageTwo} ${messageThree}`
+function printStartUpMsg() {
+  let messageOne = "You wake up to your alarm at 9:47 on a Sunday morning."
+    let messageTwo = "Through half-open eyes, you spy sunlight shining through the blinds."
+      let messageThree = "What a beautiful day to go outside."
+        let startMsg = `${messageOne} ${messageTwo} ${messageThree}`
+  alert(startMsg);
+}

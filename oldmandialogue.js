9ed6f3d3ae48e42f38e76c9dc4ffa680231a94e0
shaanmalkovich <65307797+shaanmalkovich@users.noreply.github.com>
2023-10-09 11:58:07 -0400

    Update oldmandialogue.js

--- oldmandialogue.js
+++ oldmandialogue.js
@@ -1,20 +1,37 @@
 let greetings = alert('First time?');
 let bye = alert('Enjoy your night.');
 
-let bar_line1 = "The drinks are cheap and they got some pretty lively tunes."
-let bar_line2 = `They know how to draw a crowd, but it can get rowdy sometimes.`
-let bar_line3 = `Doesn't take much to stir everyone into a frenzy.`
+let dialogue = [bar_lines, club_lines];
+
+
+let bar_talk = {
+  lineOne: "The drinks are cheap and they got some pretty lively tunes.",
+  lineTwo: "They know how to draw a crowd, but it can get rowdy sometimes.",
+  lineThree: "Doesn't take much to stir everyone into a frenzy."
+}
 
 let bar_lines = {
+  line1: firstTime,
+  line2: bar_talk.lineOne,
+  line3: bar_talk.lineTwo,
+  line4: bar_talk.lineThree,
+  line5: bye
+}
+
+let club_talk = {
+    lineOne: "I find these settings precarious; you never know what people are up to.",
+    lineTwo: "I've witnessed patrons convulse abruptly as if someone had drugged them. Enjoy your night"
+
+
+let club_lines = {
     line1: firstTime,
-    line2: bar_line1,
-    line3: bar_line2,
-    line4: bar_line3,
-    line5: bye
+    line2: club_line1,
+    line3: club_line2,
+    line4: bye
 }
 
 
-Club - "First time?" "I find these settings precarious; you never know what people are up to. I've witnessed patrons convulse abruptly as if someone had drugged them. Enjoy your night"
+Club - 
 Airport - "First time?" "I get nervous on flights. So much rides on you getting to your destination, but it mostly falls on the pilot knowing what they're doing. Enjoy your flight"
 Beach - "First time?" "If you decide to swim, watch out. The sharks here are lethal"
 Rave - "First time?" "

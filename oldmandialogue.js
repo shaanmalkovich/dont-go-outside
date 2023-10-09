79a761b1e2737d317e4bf3a996d005f5495a625f
shaanmalkovich <65307797+shaanmalkovich@users.noreply.github.com>
2023-10-09 11:47:12 -0400

    Update oldmandialogue.js

--- oldmandialogue.js
+++ oldmandialogue.js
@@ -1,4 +1,19 @@
-Bar - "First time?" "People around here can get rowdy, doesn't take much to stir everyone into a frenzy. Enjoy your night."
+let greetings = alert('First time?');
+let bye = alert('Enjoy your night.');
+
+let bar_line1 = "The drinks are cheap and they got some pretty lively tunes."
+let bar_line2 = `They know how to draw a crowd, but it can get rowdy sometimes.`
+let bar_line3 = `Doesn't take much to stir everyone into a frenzy.`
+
+let bar_lines = {
+    line1: firstTime,
+    line2: bar_line1,
+    line3: bar_line2,
+    line4: bar_line3,
+    line5: bye
+}
+
+
 Club - "First time?" "I find these settings precarious; you never know what people are up to. I've witnessed patrons convulse abruptly as if someone had drugged them. Enjoy your night"
 Airport - "First time?" "I get nervous on flights. So much rides on you getting to your destination, but it mostly falls on the pilot knowing what they're doing. Enjoy your flight"
 Beach - "First time?" "If you decide to swim, watch out. The sharks here are lethal"

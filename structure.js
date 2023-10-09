bfe712e156ab7e7b32c877d3f305edaede1949ae
shaanmalkovich <65307797+shaanmalkovich@users.noreply.github.com>
2023-10-09 01:53:13 -0400

    Update structure.js

--- structure.js
+++ structure.js
@@ -1,4 +1,4 @@
-let initialStartMsg = 'You wake up to your alarm at 9:47 on a Sunday morning. Through half-open eyes, you spy sunlight shining through the blinds. What a beautiful day to go outside."
-let choice = prompt("Where would you like to go today?")
+let initialStartMsg = alert("You wake up to your alarm at 9:47 on a Sunday morning. Through half-open eyes, you spy sunlight shining through the blinds. What a beautiful day to go outside.")
+let choice = prompt("Where would you like to go today? ")
 
 startUp();

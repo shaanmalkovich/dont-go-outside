227c7c11bf83ec9d9fa9130609e6ce74e5dee492
shaanmalkovich <65307797+shaanmalkovich@users.noreply.github.com>
2023-10-09 02:25:08 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,10 @@
+function printStartUpMsg() {
+  let messageOne = "You wake up to your alarm at 9:47 on a Sunday morning."
+    let messageTwo = "Through half-open eyes, you spy sunlight shining through the blinds."
+      let messageThree = "What a beautiful day to go outside."
+        let startMsg = `${messageOne} ${messageTwo} ${messageThree}`
+  alert(startMsg);
+}
+
+
 printStartUpMsg();
